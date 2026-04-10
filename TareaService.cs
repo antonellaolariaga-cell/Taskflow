9434e97843e98cd39f102a9af32663bbfdd3b3e7
Ramiro-Charry <ramiro.charry.analista@gmail.com>
2026-04-10 20:14:21 -0300

    update tareaservice
    commit2 listar tarea

--- TareaService.cs
+++ TareaService.cs
@@ -24,5 +24,25 @@ namespace Taskflow.Services
             Console.WriteLine("Tarea creada correctamente");
         }
 
+        // Listar tareas
+        public void ListarTareas(EstadoTarea? estadoFiltro = null)
+        {
+            var lista = estadoFiltro == null
+                ? tareas
+                : tareas.Where(t => t.Estado == estadoFiltro).ToList();
+
+            foreach (var t in lista)
+            {
+                Console.WriteLine($"ID: {t.Id}");
+                Console.WriteLine($"Título: {t.Titulo}");
+                Console.WriteLine($"Responsable: {t.Responsable}");
+                Console.WriteLine($"Estado: {t.Estado}");
+                Console.WriteLine($"Fecha creación: {t.FechaCreacion}");
+                Console.WriteLine($"Última modificación: {t.FechaModificacion}");
+                Console.WriteLine("---------------------------");
+            }
+        }
+
+        
     }
 }
