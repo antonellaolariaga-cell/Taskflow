c8c838498e056cfefdad175f461a35b94ae65155
Ramiro-Charry <ramiro.charry.analista@gmail.com>
2026-04-10 19:06:53 -0300

    Update Tarea.cs
    Creación de clases public y private

--- Tarea.cs
+++ Tarea.cs
@@ -9,5 +9,26 @@ namespace Taskflow.Models
         Completada
     }
 
-    
+    public class Tarea
+    {
+        private static int contadorId = 1;
+
+        public int Id { get; private set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Responsable { get; set; }
+        public EstadoTarea Estado { get; set; }
+        public DateTime FechaCreacion { get; private set; }
+        public DateTime? FechaModificacion { get; set; }
+
+        public Tarea(string titulo, string descripcion, string responsable)
+        {
+            Id = contadorId++;
+            Titulo = titulo;
+            Descripcion = descripcion;
+            Responsable = responsable;
+            Estado = EstadoTarea.Pendiente;
+            FechaCreacion = DateTime.Now;
+        }
+    }
 }
