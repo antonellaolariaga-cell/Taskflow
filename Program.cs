e8e66607896b6eea322f36ea53d8b2ca7ad17268
francososa-213-2027 <francososa.213@mi.unc.edu.ar>
2026-04-10 18:37:54 -0300

    Estructura inicial program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Taskflow.Services;
+using Taskflow.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TareaService servicio = new TareaService();
+
+        while (true)
+        {
+            Console.WriteLine("\n--- TASKFLOW ---");
+            Console.WriteLine("1. Crear tarea");
+            Console.WriteLine("2. Listar todas");
+            Console.WriteLine("3. Listar pendientes");
+            Console.WriteLine("4. Listar en progreso");
+            Console.WriteLine("5. Listar completadas");
+            Console.WriteLine("6. Cambiar estado");
+            Console.WriteLine("0. Salir");
+        }
+    }
+}
