53277e5536c7169886248290f5c180c7f753c3ea
Ramiro-Charry <ramiro.charry.analista@gmail.com>
2026-04-14 21:29:48 -0300

    1er commit crear tareaservice
    crear tarea

---
+++ TareaService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Taskflow.Models;
+
+namespace Taskflow.Services
+{
+    public class TareaService
+    {
+        private List<Tarea> tareas = new List<Tarea>();
+
+        // Crear tarea
+        public void CrearTarea(string titulo, string descripcion, string responsable)
+        {
+            if (string.IsNullOrEmpty(titulo))
+            {
+                Console.WriteLine("El título es obligatorio");
+                return;
+            }
+
+            var tarea = new Tarea(titulo, descripcion, responsable);
+            tareas.Add(tarea);
+
+            Console.WriteLine("Tarea creada correctamente");
+        }
+
+    }
+}
